24301f4523b9a668e77fb19ab09377614e8f8030
Philip Simpson <pwsjas433@hotmail.com>
2022-08-01 22:19:04 +0000

    Add jquery like toggling, and POST likes/id

--- public/scripts/resourceUI.js
+++ public/scripts/resourceUI.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
-  const $like = $('.likes')
-  $like.on({
+  $('.likes').on({
     mouseenter: function () {
+      const $like = $(this)
       if (!$like.children('.fa-solid').length) {
         $like.append(`<i class="fa-solid fa-heart"></i>`)
       }
     },
     //Toggle Like
     click: function () {
-      $like.off('mouseleave');
+      const $like = $(this)
+      if ($like.attr('value') === 'true') {
+        console.log('clicked')
+        $like.find(".fa-solid").remove();
+        $like.attr('value', 'false');
+        $like.on('mouseleave', function () {
+          const $like = $(this)
+          $like.children('.fa-solid').remove();
+        })
+      } else {
+        likeResource($like)
+        $like.attr('value', 'true');
+        $like.off('mouseleave');
+      }
+      console.log($like.attr('value'));
     },
     mouseleave: function () {
+      const $like = $(this)
       $like.children('.fa-solid').remove();
     }
   });
+
+
+  const likeResource = function ($resource) {
+    $resource_id = $resource.parent().parent().siblings()
+
+    $.post(`http://localhost:8080/api/likes/${$resource_id.attr('id')}`)
+      .then(function() {
+        console.log('liked');
+      });
+  }
 });
