356b5a58931943850fea410e8e11b0b0a48884f4
Philip Simpson <pwsjas433@hotmail.com>
2022-08-04 23:33:08 +0000

    Fix Merge Errors

--- public/scripts/resourceUI.js
+++ public/scripts/resourceUI.js
@@ -1,3 +1,4 @@
+const loadRatings = require('./app');
 //RESOURCE PANEL SCRIPTS
 $(document).ready(function() {
   //STAR RATINGS
@@ -53,6 +54,7 @@ $(document).ready(function() {
 
       $.post(`http://localhost:8080/api/ratings/${resource_id}`, {rating: rating})
       .then(function() {
+        loadRatings();
       });
     },
     //On MOUSE LEAVE hide / display stars based on resource rating
